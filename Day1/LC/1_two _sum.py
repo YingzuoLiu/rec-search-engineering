e69170a1feb2e2d9189f7d072e8afed2ca91a902
YingzuoLiu <156630675+YingzuoLiu@users.noreply.github.com>
2025-12-18 14:54:34 +0800

    Implement twoSum function in 1_two_sum.py

---
+++ Day1/LC/1_two _sum.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        seen={}
+        for i, x in enumerate(nums):
+            need = target - x
+            if need in seen:
+                return (seen[need],i)
+            seen[x] =i
